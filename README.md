d6502b1a990ce855d34a349668da9ac34ec8679a
Claudio Bernasconi <email@claudio-bernasconi.net>
2019-09-05 18:40:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This repository contains example code explaining how to access data via web requests in .NET Core applications. See YouTube for more details: 
+
+[URL]
---
+++ WebRequestDemo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            #region Example 1
+            var httpClient = HttpClientFactory.Create();
+
+            var url = "https://jsonplaceholder.typicode.com/todos/1";
+            var data = await httpClient.GetStringAsync(url);
+
+            Console.WriteLine(data);
+            #endregion
+
+            #region Example 2
+            //var httpClient = HttpClientFactory.Create();
+
+            //var url = "https://jsonplaceholder.typicode.com/todos/1";
+            //HttpResponseMessage httpResponseMessage =
+            //await httpClient.GetAsync(url);
+            //if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+            //{
+            //    var content = httpResponseMessage.Content;
+            //    var data = await content.ReadAsStringAsync();
+            //    Console.WriteLine(data);
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Error: " + httpResponseMessage.StatusCode);
+            //}
+            #endregion
+
+            #region Example 3
+            //var httpClient = HttpClientFactory.Create();
+
+            //var url = "https://jsonplaceholder.typicode.com/todos/1";
+            //HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
+            //if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+            //{
+            //    var content = httpResponseMessage.Content;
+            //    var data = await content.ReadAsAsync<Data>();
+
+            //    Console.WriteLine(data);
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Error: " + httpResponseMessage.StatusCode);
+            //}
+            #endregion
+        }
+    }
+
+    class Data
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+
+        public override string ToString()
+        {
+            return $"{UserId}, {Id}, {Title}, {Completed}";
+        }
+    }
+}
